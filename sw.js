6a3870551a3b51a3fa930cbab6f884ee8dd8f9fe
santanusoe <soesantanu.soe@gmail.com>
2026-05-01 15:29:04 +0530

    Add homepage service worker cache

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME='santanu-home-v1';
+const HOME_ASSETS=['/','/index.html','/images/profile.svg'];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(HOME_ASSETS)).then(()=>self.skipWaiting()));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  const request=event.request;
+  if(request.method!=='GET') return;
+  const url=new URL(request.url);
+  if(url.origin!==location.origin) return;
+
+  if(url.pathname==='/' || url.pathname==='/index.html' || url.pathname==='/images/profile.svg'){
+    event.respondWith(
+      caches.match(request).then(cached=>{
+        const network=fetch(request).then(response=>{
+          const copy=response.clone();
+          caches.open(CACHE_NAME).then(cache=>cache.put(request,copy));
+          return response;
+        }).catch(()=>cached);
+        return cached || network;
+      })
+    );
+  }
+});
