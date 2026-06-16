a5d8e031712f41fbd4b2609a142be123d1d986b5
santanusoe <soesantanu.soe@gmail.com>
2026-06-16 14:04:47 +1000

    Clean stale talkmap entries

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,62 +1,47 @@
 var addressPoints = [
   [
-    "Mini-Course on Optimization Theory and Its Applications<br />VIASM; Online",
-    43.5904719,
-    3.8595132
-  ],
-  [
-    "Workshop on Data Science: Probabilistic and Optimization Methods<br />ICTS-TIFR; Bangalore, India",
-    12.9767936,
-    77.590082
-  ],
-  [
-    "Analysis, Inverse Problems and Applications<br />Department of Mathematics, IIT Madras; Chennai, India",
-    13.0836939,
-    80.270186
-  ],
-  [
-    "National Symposium on Mathematics and Applications (NSMA)<br />Department of Mathematics, IIT Madras; Chennai, India",
-    13.0836939,
-    80.270186
-  ],
-  [
-    "Workshop on Linear Algebra through Data Science<br />IMSc; Chennai, India",
-    13.0836939,
-    80.270186
-  ],
-  [
-    "Mathematics in Industry Study Group (MISG 2025)<br />Monash University, Clayton Campus; VIC, Australia",
-    -36.5986096,
-    144.6780052
-  ],
-  [
-    "Talk 2 on Relevant Topic in Your Field<br />London School of Testing; London, UK",
-    51.5074456,
-    -0.1277653
-  ],
-  [
-    "Talk 1 on Relevant Topic in Your Field<br />UC San Francisco, Department of Testing; San Francisco, CA, USA",
-    37.7879363,
-    -122.4075201
-  ],
-  [
-    "Melbourne India Postgraduate Academy Conference (MIPA 2025)<br />University of Melbourne; Online",
-    43.5904719,
-    3.8595132
+    "Workshop on Decision and Control<br />University of Melbourne; Melbourne, Australia",
+    -37.7963,
+    144.9614
   ],
   [
     "5th International Conference on Nonlinear Applied Analysis and Optimization<br />IIT Jodhpur; Rajasthan, India",
-    26.8105777,
-    73.7684549
+    26.8106,
+    73.7685
   ],
   [
-    "Tutorial 1 on Relevant Topic in Your Field<br />UC-Berkeley Institute for Testing Science; Berkeley, CA, USA",
-    37.8708393,
-    -122.272863
+    "Melbourne India Postgraduate Academy Conference (MIPA 2025)<br />University of Melbourne; Online",
+    -37.7963,
+    144.9614
   ],
   [
-    "Conference Proceeding talk 3 on Relevant Topic in Your Field<br />Testing Institute of America 2014 Annual Conference; Los Angeles, CA, USA",
-    34.0536909,
-    -118.242766
+    "Mathematics in Industry Study Group (MISG 2025)<br />Monash University; Melbourne, Australia",
+    -37.9140,
+    145.1300
+  ],
+  [
+    "Workshop on Linear Algebra through Data Science<br />IMSc; Chennai, India",
+    13.0837,
+    80.2702
+  ],
+  [
+    "Mini-Course on Optimization Theory and Its Applications<br />VIASM; Online",
+    21.0285,
+    105.8542
+  ],
+  [
+    "Workshop on Data Science: Probabilistic and Optimization Methods<br />ICTS-TIFR; Bangalore, India",
+    12.9768,
+    77.5901
+  ],
+  [
+    "NSMA 2022<br />Department of Mathematics, IIT Madras; Chennai, India",
+    13.0837,
+    80.2702
+  ],
+  [
+    "Analysis, Inverse Problems and Applications<br />Department of Mathematics, IIT Madras; Chennai, India",
+    13.0837,
+    80.2702
   ]
 ];
