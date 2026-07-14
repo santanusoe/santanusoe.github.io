d18a275a28d41989429d4494a2df56c4828bc6c1
santanusoe <soesantanu.soe@gmail.com>
2026-07-14 14:22:59 +1000

    Remove stale homepage caching
    * Unregister stale homepage service workers
    
    * Remove service worker page caching

--- sw.js
+++ sw.js
@@ -1,32 +1,17 @@
-const CACHE_NAME='santanu-home-v25';
-const HOME_ASSETS=['/','/index.html','/images/profile.svg'];
-
-self.addEventListener('install',event=>{
-  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(HOME_ASSETS)).then(()=>self.skipWaiting()));
+self.addEventListener("install", function () {
+  self.skipWaiting();
 });
 
-self.addEventListener('activate',event=>{
+self.addEventListener("activate", function (event) {
   event.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch',event=>{
-  const request=event.request;
-  if(request.method!=='GET') return;
-  const url=new URL(request.url);
-  if(url.origin!==location.origin) return;
-
-  if(url.pathname==='/' || url.pathname==='/index.html' || url.pathname==='/images/profile.svg'){
-    event.respondWith(
-      caches.match(request).then(cached=>{
-        const network=fetch(request).then(response=>{
-          const copy=response.clone();
-          caches.open(CACHE_NAME).then(cache=>cache.put(request,copy));
-          return response;
-        }).catch(()=>cached);
-        return cached || network;
+    caches.keys()
+      .then(function (keys) {
+        return Promise.all(keys.map(function (key) {
+          return caches.delete(key);
+        }));
       })
-    );
-  }
+      .then(function () {
+        return self.clients.claim();
+      })
+  );
 });
